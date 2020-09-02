da0ebbc2ca54eac85200a681523c808b691801c9
james HE <james840525@gmail.com>
2020-09-02 17:40:03 +0800

    sec

---
+++ public/js/all.js
@@ -0,0 +1,28 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.scrollTop').click(function (e) {
+    e.preventDefault();
+    var target = $(this).attr('href');
+    var targetPos = $(target).offset().top; // $('html,body').animate({
+    //     scrollTop: targetPos - 55
+    // }, 1000);
+  });
+  $(window).scroll(function () {
+    var scrollPos = $(window).scrollTop();
+    var windowHeight = $(window).height(); // $('.animate').each(function () {
+    //     var thisPos = $(this).offset().top;
+    //     if (windowHeight + scrollPos >= thisPos) {
+    //         $(this).addClass('fade-in');
+    //     }
+    // });
+    // $('.slide_animate').each(function () {
+    //     var slideTop = $('.slide_animate').offset().top;
+    //     if (windowHeight + scrollPos >= slideTop) {
+    //         $('.slide_animate').addClass('slideIn');
+    //     }
+    // })
+    // $('.parallax').css('background-position-y', -(scrollPos / 0.5) + 'px')        
+  });
+});
+//# sourceMappingURL=all.js.map
